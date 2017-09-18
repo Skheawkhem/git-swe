9999d2f27e1d3d35a5c604fb07553cae1532d25e
M.mahdee <mahdee.dark@gmail.com>
2017-09-18 14:11:59 +0700

    create main checkeven

---
+++ src/Checkeven.java
@@ -0,0 +1,14 @@
+
+public class Checkeven {
+	
+	
+	public int num ;
+	
+	public  Checkeven(int num) {
+		this.num = num;
+	}
+	public double OddEven(int num) {
+		double a = num*num*22/7;
+		return a;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+	import java.util.Scanner;
+
+	public class Main {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Scanner in = new Scanner(System.in);
+			int num = in.nextInt();
+			
+			Checkeven sum = new Checkeven(num);
+			double a =sum.OddEven(num);
+			System.out.println(a);
+		}
+	}
